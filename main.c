6588101a90547c58680571b38cc9d5769f2c1d5e
Valdemar22848 <122788990+Valdemar22848@users.noreply.github.com>
2024-02-22 18:18:57 +0300

    Добавление кода по пункту №3

---
+++ main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    scanf("%d", &x);
+    int y = 5 * x + 20;
+     printf("You entered: %d", y);
+
+    return 0;
+}
